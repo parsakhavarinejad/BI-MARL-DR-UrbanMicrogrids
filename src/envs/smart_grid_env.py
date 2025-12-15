135dbe9d86c83ba43e2b121ef027a6f0a6b105fa
Parsa Khavarinejad <parsa.khavarinejad@gmail.com>
2025-12-15 16:46:39 +0330

    Added Env

---
+++ src/envs/smart_grid_env.py
@@ -0,0 +1,82 @@
+import gymnasium as gym
+import numpy as np
+from gymnasium import spaces
+
+
+class SmartGridEnv(gym.Env):
+    def __init__(self, data_loader):
+        super().__init__()
+        self.data_loader = data_loader
+        self.num_agents = 12
+
+        self.action_space = spaces.Box(
+            low=-1.0, high=1.0, shape=(self.num_agents,), dtype=np.float32
+        )
+
+        self.observation_space = spaces.Box(
+            low=-np.inf, high=np.inf, shape=(self.num_agents, 3), dtype=np.float32
+        )
+
+        self.current_step = 0
+        self.day_data = None
+
+        self.expected_load = (
+            self.data_loader.daily_episodes[:, :, :, 0].sum(axis=1).mean(axis=0)
+        )
+
+        self.total_steps = 96
+
+    def reset(self, seed=None, options=None):
+        super().reset(seed=seed)
+        day_idx = np.random.randint(len(self.data_loader))
+        self.day_data = self.data_loader.get_episode(day_idx)
+
+        self.current_step = 0
+        return self._get_obs(), {}
+
+    def _get_dynamic_price(self, total_grid_load, base_price, alpha=0.5):
+
+        total_price = base_price * (
+            1 + alpha * (total_grid_load / self.expected_load[self.current_step]) ** 2
+        )
+        return total_price
+
+    def step(self, actions):
+        obs = self._get_obs()
+        current_base_load, current_base_price, time_feature = obs[0], obs[1], obs[2]
+        actual_load = current_base_load * (1 + actions)
+        total_grid_load = np.sum(actual_load, axis=0)
+        current_price = self._get_dynamic_price(total_grid_load, current_base_price)
+
+        rewards = []
+        for agent in range(self.num_agents):
+            cost = actual_load[agent] * current_price[agent]
+            discomfort = (actions[agent]) ** 2
+
+            reward = -(cost + discomfort)
+            rewards.append(reward)
+
+        self.current_step += 1
+        done = self.current_step >= self.total_steps
+
+        next_obs = (
+            self._get_obs()
+            if not done
+            else np.zeros((self.num_agents, 3), dtype=np.float32)
+        )
+
+        return next_obs, np.array(rewards, dtype=np.float32), done, False, {}
+
+    def _get_obs(self):
+
+        current_values_feature = self.day_data[:, self.current_step, :]
+        current_base_load = current_values_feature[:, 0]
+        current_base_price = current_values_feature[:, 2]
+
+        time_feature = np.full((self.num_agents,), self.current_step / self.total_steps)
+
+        return np.stack(
+            [current_base_load, current_base_price, time_feature],
+            axis=1,
+            dtype=np.float32,
+        )
--- src/utils/data_loader.py
+++ src/utils/data_loader.py
@@ -3,29 +3,43 @@ import numpy as np
 
 
 class SmartGridDataLaoder:
+    """
+    In this class we preprocess the data and turn it to different episodes,
+    we reshape the data to (num_homes, length of the episode, the number of features)
+
+    In our first experiment the reshaped data has (12, 96, 3)
+
+    class SmartGridDataLoader
+        input (str): the data path - in csv.gz format
+        output (np.array): daily episode per index
+        core method: get_episode
+    """
+
     def __init__(self, data_path):
         print(f"Loading the data from {data_path}")
-        self.df = pd.raed(data_path, compression='gzip')
+        self.df = pd.read_csv(data_path, compression="gzip")
 
-        self.df['ts'] = pd.to_datetime(self.df['ts'])
-        self.df['date'] = self.df['ts'].dt.date.astype('str')
+        self.df["ts"] = pd.to_datetime(self.df["ts"])
+        self.df["date"] = self.df["ts"].dt.date.astype("str")
 
-        self.df = self.df.sort_values(by=['date', 'homeid', 'ts'])
-        self.home_uinque = sorted(self.df['homeid'].unique())
-        self.date_unique = sorted(self.df['date'].unique())
+        self.df = self.df.sort_values(by=["date", "homeid", "ts"])
+        self.home_uinque = sorted(self.df["homeid"].unique())
+        self.date_unique = sorted(self.df["date"].unique())
 
-        print(f"There are {len(self.home_uinque)} homes in total and {len(self.date_unique)} days!")
+        print(
+            f"There are {len(self.home_uinque)} homes in total and {len(self.date_unique)} days!"
+        )
 
         self.daily_episodes = []
         self.valid_date = []
 
-        self.feature_cols = ['e_kwh', 'p_w_mean', 'unit_charge_pence_per_kwh']
+        self.feature_cols = ["e_kwh", "p_w_mean", "unit_charge_pence_per_kwh"]
 
         self._process_episodes()
 
     def _process_episodes(self):
 
-        episode_len = 96 #we have 96 numbers of 15min slice in a day
+        episode_len = 96  # we have 96 numbers of 15min slice in a day
         num_homes = len(self.date_unique)
 
         expected_rows = episode_len * num_homes
@@ -34,27 +48,28 @@ class SmartGridDataLaoder:
 
         for date in self.date_unique:
 
-            df_dates = self.df[self.df['date'] == date]
+            df_dates = self.df[self.df["date"] == date]
             if len(df_dates) != expected_rows:
-                print(f'Skipping incomplete date for {date}')
+                print(f"Skipping incomplete date for {date}")
                 continue
-            
+
             values = df_dates[self.feature_cols].values
 
             try:
-                episode_matrix = values.reshape(num_homes, episode_len, len(self.feature_cols))
+                episode_matrix = values.reshape(
+                    num_homes, episode_len, len(self.feature_cols)
+                )
                 valid_episode.append(episode_matrix)
                 self.valid_date.append(date)
             except ValueError as e:
                 print(f"Processing failed for {date}: {e}")
-        
+
         self.daily_episodes = np.array(valid_episode)
         print(f"Successfully prepared {len(self.daily_episodes)} community episodes.")
 
     def get_episode(self, index):
         """Returns (homes, steps, features) for a specific day index."""
         return self.daily_episodes[index]
-    
+
     def __len__(self):
         return len(self.daily_episodes)
-
